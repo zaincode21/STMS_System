fd167a2682f2595cbc2b06b99b445ac4ce1afa55
zaincode21 <sergedukuziyaremye@gmail.com>
2025-06-04 06:43:30 +0200

    creation controller

--- schoool-management-system/src/main/java/net/javaguides/sms/entity/student.java
+++ schoool-management-system/src/main/java/net/javaguides/sms/entity/student.java
@@ -1,5 +1,67 @@
 package net.javaguides.sms.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="students")
+
 public class student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="first_name", nullable = false)
+	private String firstname;
+	
+	@Column(name="last_name")
+	private String lastname;
+	
+	@Column(name="email")
+	private String email;
+	
+	public student() {
+		
+		
+	}
+	
+	
+	public student(String firstname, String lastname, String email) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 
 }
